56549cfb996bdd7a20f7ec2ac32be4e9b73c6bd1
nathanielbhal <147010214+nathanielbhal@users.noreply.github.com>
2023-10-04 20:07:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// Crea la variable de referncia del lienzo usando fabric.Canvas()
+
+
+//Define el ancho inicial y el alto del cloque de imágenes 
+
+
+//Define el inicio de las coordenadas  y Y del jugador 
+
+
+
+// Define una variable llamada player_object para almacenar la imagen del jugador
+
+
+// Agrega una función llamada player_update() para agregar la imagen del jugador 
+
+
+
+
+
+
+
+// Agrega una función llamada new_image() para agregar las diferentes imágene al presionar teclas específicas.
+
